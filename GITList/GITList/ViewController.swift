6d4beac56421e57090bec3c0f7ea1c514b287039
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-08-21 19:48:00 -0700

    initial commit

---
+++ GITList/GITList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GITList
+//
+//  Created by Brandon Holderman on 8/21/17.
+//  Copyright © 2017 Brandon Holderman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

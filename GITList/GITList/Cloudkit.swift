8456a3ec4655abdceb03e4c2030714a95b10ff66
Redhatfour1 <yaloumessenger@yahoo.com>
2017-08-24 20:16:04 -0700

    bringing tree up to date

--- GITList/GITList/Cloudkit.swift
+++ GITList/GITList/Cloudkit.swift
@@ -1,26 +1,26 @@
-////
-////  Cloudkit.swift
-////  GITList
-////
-////  Created by Louis W. Haywood on 8/23/17.
-////  Copyright © 2017 Brandon Holderman. All rights reserved.
-////
 //
-//import Foundation
-//import CloudKit
+//  Cloudkit.swift
+//  GITList
 //
-//typealias ListCompletion = (Bool)->()
-//typealias GetListCompletion = ([List]?)->()
+//  Created by Louis W. Haywood on 8/23/17.
+//  Copyright © 2017 Brandon Holderman. All rights reserved.
 //
-//class CloudKit {
-//    static let shared = CloudKit()
-//    let container = CKContainer.default()
-//    var database : CKDatabase {
-//        return container.privateCloudDatabase
-//    }
-//    
-//    private init (){}
-//    
+
+import Foundation
+import CloudKit
+
+typealias ListCompletion = (Bool)->()
+typealias GetListCompletion = ([List]?)->()
+
+class CloudKit {
+    static let shared = CloudKit()
+    let container = CKContainer.default()
+    var database : CKDatabase {
+        return container.privateCloudDatabase
+    }
+    
+    private init (){}
+    
 //    func save(list: List, completion: @escaping ListCompletion) {
 //        do {
 //            if let record = try list.record() {
@@ -52,22 +52,22 @@
 //            }
 //            if let records = records {
 //                var allList = [List]()
-//                for record in records {
-//                    
-//                    guard let asset = record["image"] as? CKAsset else { continue }
-//                    let path = asset.fileURL.path
-//                    
-//                    guard let image = UIImage(contentsOfFile: path) else { continue }
-//                    let newList = List(image: image)
-//                    userList.append(newList)
-//                }
-//                
-//                OperationQueue.main.addOperation {
-//                    completion(userList)
-//                }
+////                for record in records {
+////                    
+////                    guard let asset = record["image"] as? CKAsset else { continue }
+////                    let path = asset.fileURL.path
+////                    
+////                    guard let image = UIImage(contentsOfFile: path) else { continue }
+////                    let newList = List(image: image)
+////                    userList.append(newList)
+////                }
+////                
+////                OperationQueue.main.addOperation {
+////                    completion(userList)
+////                }
 //            } else {
 //                completion(nil)
 //            }
 //        }
 //    }
-//}
+}
